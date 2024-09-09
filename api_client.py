271397eb4fa00cf3a327aba3adf26a1c21184663
Unkas2024 <unkas356@gmail.com>
2024-09-09 22:27:21 +0300

    Настройка API клиента

---
+++ api_client.py
@@ -0,0 +1,88 @@
+from http.client import responses
+
+import request
+import os
+
+from django.utils.html import urlize
+from dotenv import load_dotenv
+from core.settings.environments import Environment
+from core.clients.endpoints import Endpoints
+#from core.settings.config import Users, Timeout
+import allure
+
+load_dotenv()
+
+
+class APIClient:
+    def __init__(self):
+        environment_str = os.getenv('ENVIRONMENT')
+        try:
+            environment = Environment[environment_str]
+        except KeyError:
+            raise ValueError(f"Unsuported environment value: {environment_str}")
+
+        self.base_url = self.get_base_url(environment)
+        self.session = request.Session()
+        self.session.headers = {
+            'Content-Type': 'application/json'
+        }
+
+    def get_base_url(self, environment: Environment) -> str:
+        if environment == Environment.TEST:
+            return os.getenv('TEST_BASE_URL')
+        elif environment == Environment.PROD:
+            return os.getenv('PROD_BASE_URL')
+        else:
+            raise ValueError(f"Usupported environment: {environment}")
+
+
+#    def get(self, endpoint, params=None, status_code=200):
+#        url = self.base_url + endpoint
+#        response = request.get(url, headers=self.headers, params=params)
+
+#    if status.code:
+#        assert response.status_code == status_code
+#    return response.json()
+
+    #    def post(self, endpoint, data=None, status_code=200):
+    #    url = self.base_url + endpoint
+    #    response = request.get(url, headers=self.headers, json=data)
+    #    if status.code:
+    #        assert response.status_code == status_code
+    #    return response.json()
+
+#    def ping(self):
+#        with allure.step('Ping api client'):
+#            url = f"{self.base_url}{PING_ENDPOINT.value}"
+#            response = self.session.get(url)
+#            print(url)
+#            response.raise_for_status()
+#        with allure.steps('Assert status code'):
+#            assert response.status_code == 201, f"Expected status 201 but got {response.status_code}"
+#        return response.status_code
+
+#    def auth(self):
+#        with allure.step('Getting authenticate'):
+#            url = f"{self.base_url}{AUTH_ENDPOINT.value}"
+#             payload = {"username": Users.USER_NAME.value, "password": Users.Password.value}
+#            response = self.session.post(url, json=payload, timeout=Timeouts.TIMEOUT.value)
+#            response.raise_for_status ()
+#        with allure.steps('Checking status code'):
+#            assert response.status_code == 200, f"Expected status 200 but got {response.status_code}"
+#          token = response.json().get ("token")
+#        with allure.steps('Updating header with authorization'):
+#            self.session.headers.update({"Authorization": f"Bearer" {token}""})
+
+#    def get_booking_by_id(self):
+#        url = f"{self.base_url}{Endpoints.BOOKING_ID_ENDPOINT.value}"
+#        response = self.session.get (url)
+#        response.raise_for_status()
+#        assert response.status_code == 200
+
+
+#    def delete_booking(self):
+#        url = f"{self.base_url}{Endpoints.BOOKING_ID_ENDPOINT.value}"
+#        response = self.session.delete(url, auth = )
+#        response.raise_for_status()
+#        assert response.status_code == 200
+print(APIClient.get_base_url())
